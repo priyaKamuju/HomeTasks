cd42adc09b5560d848307401973c76cffc6efbf5
priyaKamuju <102951621+priyaKamuju@users.noreply.github.com>
2022-04-19 10:32:09 +0530

    Add files via upload

---
+++ Assignment_2.1/BasicMathematicalOperations.js
@@ -0,0 +1,8 @@
+function basicOp(operation, value1, value2)
+{
+  if(operation == '+') return value1 + value2;
+  if(operation == '-') return value1 - value2;
+  if(operation == '*') return value1 * value2;
+  return value1 / value2;
+  // Code
+}
